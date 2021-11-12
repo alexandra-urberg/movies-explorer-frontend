bdf91a2f13a45616a93b558f698e587165423e21
Sasha Stepanova <chernishevskie@yandex.ru>
2021-11-12 20:01:04 +0200

    feat: added validation and needed properties

--- src/components/moviesTemplate/MoviesTemplate.js
+++ src/components/moviesTemplate/MoviesTemplate.js
@@ -1,16 +1,39 @@
-import React from "react";
+import { useState } from "react";
 
 function MoviesTemplate() {
+  const [movie, setMovie] = useState("");
+  const [validationErrors, setValidationErrors] = useState({ movie: "" }); //state of input validation
+
+  function handleChangeMovie(e) {
+    //Обработчик изменения инпута name обновляет стейт
+    const { value } = e.target;
+    setMovie(value);
+
+    if (value.length < 2) {
+      validationErrors.movie = "Нужно ввести ключевое слово";
+    } else {
+      validationErrors.movie = "" && setValidationErrors(validationErrors);
+    }
+  }
   return (
     <div className="moviesTemplate">
       <form className="moviesTemplate__form">
         <label className="moviesTemplate__label">
           <input
+            onChange={handleChangeMovie}
+            type="text"
+            value={movie}
+            name="movie"
             className="moviesTemplate__input moviesTemplate__input-size moviesTemplate__image"
             placeholder="Фильмы"
+            required
           />
-          <span className="moviesTemplate__input-error"></span>
           <button className="moviesTemplate__button" type="submit"></button>
+          <span
+            className={`${validationErrors.movie ? "form__input-error" : null}`}
+          >
+            {validationErrors.movie}
+          </span>
         </label>
       </form>
       <div className="moviesTemplate__search">
