9597cf45c197138883b6921bc473241b693a026e
Sasha Stepanova <chernishevskie@yandex.ru>
2021-11-23 00:29:22 +0200

    refactor: navigation header

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -111,54 +111,59 @@ function Navigation(props) {
             </li>
           </MediaQuery>
         </Route>
-        {props.isAuthorized ? (
-          <Route path="/">
-            <MediaQuery minWidth={841}>
+        <Route path="/">
+          {props.isAuthorized ? (
+            <>
+              <MediaQuery minWidth={841}>
+                <li className="header__link-navigation">
+                  <Link to="/movies" className="header__link header__font-size">
+                    Фильмы
+                  </Link>
+                </li>
+                <li className="header__link-navigation">
+                  <Link
+                    to="/saved-movies"
+                    className="header__link header__font-size"
+                  >
+                    Сохраненные фильмы
+                  </Link>
+                </li>
+                <li className="header__link-navigation">
+                  <Link
+                    to="/profile"
+                    className="header__link header__font-size"
+                  >
+                    Аккаунт
+                  </Link>
+                </li>
+                <li className="header__container">
+                  <div className="header__icon"></div>
+                </li>
+              </MediaQuery>
+              <MediaQuery maxWidth={840}>
+                <li className="header__sandwich">
+                  <button
+                    onClick={props.onPopupOpen}
+                    className="header__sandwich-icon"
+                  ></button>
+                </li>
+              </MediaQuery>
+            </>
+          ) : (
+            <MediaQuery maxWidth={1280}>
               <li className="header__link-navigation">
-                <Link to="/movies" className="header__link header__font-size">
-                  Фильмы
+                <Link to="/sign-up" className="header__link">
+                  Регистрация
                 </Link>
               </li>
               <li className="header__link-navigation">
-                <Link
-                  to="/saved-movies"
-                  className="header__link header__font-size"
-                >
-                  Сохраненные фильмы
+                <Link to="/sign-in" className="header__link header__login">
+                  Войти
                 </Link>
-              </li>
-              <li className="header__link-navigation">
-                <Link to="/profile" className="header__link header__font-size">
-                  Аккаунт
-                </Link>
-              </li>
-              <li className="header__container">
-                <div className="header__icon"></div>
               </li>
             </MediaQuery>
-            <MediaQuery maxWidth={840}>
-              <li className="header__sandwich">
-                <button
-                  onClick={props.onPopupOpen}
-                  className="header__sandwich-icon"
-                ></button>
-              </li>
-            </MediaQuery>
-          </Route>
-        ) : (
-          <Route path="/">
-            <li className="header__link-navigation">
-              <Link to="/sign-up" className="header__link">
-                Регистрация
-              </Link>
-            </li>
-            <li className="header__link-navigation">
-              <Link to="/sign-in" className="header__link header__login">
-                Войти
-              </Link>
-            </li>
-          </Route>
-        )}
+          )}
+        </Route>
       </Switch>
     </ul>
   );
