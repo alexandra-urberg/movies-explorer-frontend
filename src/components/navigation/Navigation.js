48362bb77b4135d38740e0685ba9b6949e7b765c
Sasha Stepanova <chernishevskie@yandex.ru>
2021-11-22 02:40:11 +0200

    refactor: changed navigation for header if user is authorized

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -35,7 +35,10 @@ function Navigation(props) {
           </MediaQuery>
           <MediaQuery maxWidth={840}>
             <li className="header__sandwich">
-              <button onClick={props.onPopupOpen} className="header__sandwich-icon"></button>
+              <button
+                onClick={props.onPopupOpen}
+                className="header__sandwich-icon"
+              ></button>
             </li>
           </MediaQuery>
         </Route>
@@ -68,7 +71,10 @@ function Navigation(props) {
           </MediaQuery>
           <MediaQuery maxWidth={840}>
             <li className="header__sandwich">
-            <button onClick={props.onPopupOpen} className="header__sandwich-icon"></button>
+              <button
+                onClick={props.onPopupOpen}
+                className="header__sandwich-icon"
+              ></button>
             </li>
           </MediaQuery>
         </Route>
@@ -98,22 +104,61 @@ function Navigation(props) {
           </MediaQuery>
           <MediaQuery maxWidth={840}>
             <li className="header__sandwich">
-              <button onClick={props.onPopupOpen} className="header__sandwich-icon"></button>
+              <button
+                onClick={props.onPopupOpen}
+                className="header__sandwich-icon"
+              ></button>
             </li>
           </MediaQuery>
         </Route>
-        <Route path="/">
-          <li className="header__link-navigation">
-            <Link to="/sign-up" className="header__link">
-              Регистрация
-            </Link>
-          </li>
-          <li className="header__link-navigation">
-            <Link to="/sign-in" className="header__link header__login">
-              Войти
-            </Link>
-          </li>
-        </Route>
+        {props.isAuthorized ? (
+          <Route path="/">
+            <MediaQuery minWidth={841}>
+              <li className="header__link-navigation">
+                <Link to="/movies" className="header__link header__font-size">
+                  Фильмы
+                </Link>
+              </li>
+              <li className="header__link-navigation">
+                <Link
+                  to="/saved-movies"
+                  className="header__link header__font-weight header__font-size"
+                >
+                  Сохраненные фильмы
+                </Link>
+              </li>
+              <li className="header__link-navigation">
+                <Link to="/profile" className="header__link header__font-size">
+                  Аккаунт
+                </Link>
+              </li>
+              <li className="header__container">
+                <div className="header__icon"></div>
+              </li>
+            </MediaQuery>
+            <MediaQuery maxWidth={840}>
+              <li className="header__sandwich">
+                <button
+                  onClick={props.onPopupOpen}
+                  className="header__sandwich-icon"
+                ></button>
+              </li>
+            </MediaQuery>
+          </Route>
+        ) : (
+          <Route path="/">
+            <li className="header__link-navigation">
+              <Link to="/sign-up" className="header__link">
+                Регистрация
+              </Link>
+            </li>
+            <li className="header__link-navigation">
+              <Link to="/sign-in" className="header__link header__login">
+                Войти
+              </Link>
+            </li>
+          </Route>
+        )}
       </Switch>
     </ul>
   );
